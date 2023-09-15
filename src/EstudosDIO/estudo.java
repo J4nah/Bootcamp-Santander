f916ce2afd99ea76241a1cc196b4cd59dba5800c
Janaina Gomes <jana.gomes90@hotmail.com>
2023-09-15 18:44:29 -0300

    up

---
+++ src/EstudosDIO/estudo.java
@@ -0,0 +1,14 @@
+package EstudosDIO;
+
+public class estudo {
+
+	public static void main(String[] args) {
+
+		for (int i = 0; i <= 10; i++) {
+			
+			int tabuada = i * 10;
+			System.out.println("tabuada 10 X " + i + " = " + tabuada);
+
+		}
+	}
+}
